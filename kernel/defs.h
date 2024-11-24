f34c8cb18bd81fcbf64d6fac74fa46796ed3f210
anhthi2714 <152952446+anhthi2714@users.noreply.github.com>
2024-11-24 13:53:08 +0700

    Update defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -185,5 +185,8 @@ void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
 void            virtio_disk_intr(void);
 
+// vm.c
+void            vmprint(pagetable_t pagetable, int level);
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
